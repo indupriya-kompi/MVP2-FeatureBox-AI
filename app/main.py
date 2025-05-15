1d422e795026bf4e3f186043f75e81267033b2e9
HarshiMuktevi <harshitamuktevi@gmail.com>
2025-05-15 15:56:12 -0700

    Added decompressing files

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.responses import JSONResponse
+from zipfile import ZipFile, BadZipFile
 
 app = FastAPI()
 
@@ -9,16 +10,20 @@ async def root():
 
 @app.post("/upload/")
 async def upload_zip(file: UploadFile = File(...)):
-    # Checks for file extension
     if not file.filename.endswith(".zip"):
         raise HTTPException(status_code=400, detail="Only ZIP files are allowed.")
 
-    # Read contents in memory
-    contents = await file.read()
+    try:
+        with ZipFile(file.file) as zip_ref:
+            file_list = zip_ref.namelist()  # list of all files inside the ZIP
+            file_count = len(file_list)     # just count them
 
+    except BadZipFile:
+        raise HTTPException(status_code=400, detail="Invalid or corrupt ZIP file.")
 
     return JSONResponse(content={
         "status": "success",
         "filename": file.filename,
-        "message": "ZIP file received successfully"
+        "file_count": file_count,
+        "message": f"ZIP file opened successfully with {file_count} files inside"
     })
